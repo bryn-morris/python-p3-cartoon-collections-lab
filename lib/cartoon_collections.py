505bf417888f7dc60c05a6c9409eba2536d26944
Brynfor Morris <brynfor.morris@gmail.com>
2023-03-14 09:33:50 -0500

    first two deliverables

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,14 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarf_list):
 
-def summon_captain_planet():
-    pass
+    [print(f"{dwarf_list.index(dwarf)+1}. {dwarf}") for dwarf in dwarf_list] 
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planateer_calls_list):
+    return [f"{each_planateer_call.capitalize()}!" for each_planateer_call in planateer_calls_list]
+
+def long_planeteer_calls(list_of_calls):
+    
+    length_list = [len(eachCall) for eachCall in list_of_calls]
+
+    
+    return (if length_list)
+        
+
+
 
 def find_the_cheese():
     pass
 
+#The Testing List
 
-# TESTING
+long_planeteer_calls(["dog", "go", "top"])
